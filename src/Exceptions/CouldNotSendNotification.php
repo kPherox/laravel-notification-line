06012b9f05df434930efa5abacdfddbc891d38ac
kPherox <admin@mail.kr-kp.com>
2018-06-26 09:27:42 +0900

    Change CouldNotSendNotification to correspond to error response

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,10 +2,28 @@
 
 namespace NotificationChannels\Line\Exceptions;
 
+use LINE\LINEBot\Exception\CurlExecutionException;
+use LINE\LINEBot\Response;
+
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    public static function serviceRespondedWithAnError(Response $response)
     {
-        return new static("Descriptive error message.");
+        $responseBody = $response->getJSONDecodedBody();
+        $errorMsg = $response->getHeader('X-Line-Request-Id').": ".$responseBody['message'];
+
+        if (preg_match('/The request body has [\d]+ errors?/', $responseBody['message'], $matches)) {
+            foreach ($responseBody['details'] as $detail) {
+                $errorMsg .= PHP_EOL.$detail['message'];
+                $errorMsg .= PHP_EOL.$detail['property'];
+            }
+        }
+
+        return new static($errorMsg);
+    }
+
+    public static function curlError(CurlExecutionException $exeption)
+    {
+        return new static("LINEBot Curl error: ".$exeption->getMessage());
     }
 }
